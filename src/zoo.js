81f42a3cf84f7e70c080eab249f3ca93823f1ad6
Lucas Accurcio <lucas.accurcio@gmail.com>
2021-08-04 18:13:44 -0300

    Correções lint

--- src/zoo.js
+++ src/zoo.js
@@ -1,23 +1,23 @@
-/*eslint max-len: ["error", { "ignorePattern": "^\\s*var\\s.+=\\s*require\\s*\\(" }]*/
-
 const data = require('./data');
 
-const { species } = data;
+const { especies } = data;
 
 function getSpeciesByIds(ids) {
   // seu código aqui
   if (ids === undefined) return [];
-  return species.filter((value) => (value.id === ids));
+  return especies.filter((value) => (value.id === ids));
 }
-//console.log(getSpeciesByIds('bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'));
+// console.log(getSpeciesByIds('bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'));
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
-  const newArray = species.filter((value) => (value.name === animal)).map((value) => value.residents);
+  const newArray = especies.filter((value) => (value.name === animal));
+  newArray.map((value) => value.residents);
   return newArray[0].every((idade) => idade.age > age);
 }
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+
 }
 
 function createEmployee(personalInfo, associatedWith) {
