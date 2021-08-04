c84c46b67bae126cdac3ff4628974a241271b0c7
Lucas Accurcio <lucas.accurcio@gmail.com>
2021-08-04 18:08:41 -0300

    Requisito 02

--- src/zoo.js
+++ src/zoo.js
@@ -1,15 +1,19 @@
+/*eslint max-len: ["error", { "ignorePattern": "^\\s*var\\s.+=\\s*require\\s*\\(" }]*/
+
 const data = require('./data');
 
 const { species } = data;
 
 function getSpeciesByIds(ids) {
   // seu código aqui
-  // if (ids === undefined) return [];
-  return species.filter((value) => value.id === ids).map((specie) => specie.residents);
+  if (ids === undefined) return [];
+  return species.filter((value) => (value.id === ids));
 }
-console.log(getSpeciesByIds('bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'));
+//console.log(getSpeciesByIds('bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'));
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const newArray = species.filter((value) => (value.name === animal)).map((value) => value.residents);
+  return newArray[0].every((idade) => idade.age > age);
 }
 
 function getEmployeeByName(employeeName) {
