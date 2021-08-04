ceb67f7a78398a4d801b6ac4c9f3a5d95dc4d4fa
Lucas Accurcio <lucas.accurcio@gmail.com>
2021-08-04 16:37:29 -0300

    meio requisito 01 rs

--- src/zoo.js
+++ src/zoo.js
@@ -1,13 +1,15 @@
 const data = require('./data');
 
+const { species } = data;
+
 function getSpeciesByIds(ids) {
   // seu código aqui
-
+  // if (ids === undefined) return [];
+  return species.filter((value) => value.id === ids).map((specie) => specie.residents);
 }
-
+console.log(getSpeciesByIds('bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'));
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
-  
 }
 
 function getEmployeeByName(employeeName) {
