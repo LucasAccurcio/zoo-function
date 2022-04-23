d9fc6ddcacfb3a9ffbf8846db2b240fa3695e030
LucasAccurcio <lucas.accurcio@gmail.com>
2022-04-23 11:36:22 -0300

    docs: Alteração do README e remoção dos arquivos da Trybe

--- src/zoo.js
+++ src/zoo.js
@@ -3,31 +3,26 @@ const data = require('./data');
 const { species, employees, prices, hours } = require('./data');
 
 function getSpeciesByIds(...ids) {
-  // seu código aqui
   if (!ids) return [];
   return species.filter(({ id }) => ids.includes(id));
 }
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
   const newArray = species.find((animais) => (animais.name === animal));
   return newArray.residents.every((idade) => idade.age > age);
 }
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
   if (!employeeName) return {};
   return employees.find((emp) => emp.firstName === employeeName || emp.lastName === employeeName);
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
   const newEmployee = { ...personalInfo, ...associatedWith };
   return newEmployee;
 }
 
 function isManager(id) {
-  // seu código aqui
   const employee = employees.find((value) => value.id === id);
   if (employee.firstName === 'Stephanie') return true;
   if (employee.firstName === 'Emery') return true;
@@ -37,7 +32,6 @@ function isManager(id) {
 }
 
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
-  // seu código aqui
   const newEmployee = {
     id,
     firstName,
@@ -49,21 +43,18 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
 }
 
 function countAnimals(especies) {
-  // seu código aqui
   if (!especies) {
     const name = species.reduce((acc, value) => {
       acc[value.name] = value.residents.length;
       return acc;
     }, {});
     return name;
-    // return species.reduce((acc, element) => acc + element.residents.length, 0);
   }
   const animal = species.find((value) => value.name === especies);
   return animal.residents.length;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
   if (!entrants) return 0;
   if (Object.keys(entrants).length === 0) return 0;
   const { Adult = 0, Senior = 0, Child = 0 } = entrants;
@@ -123,7 +114,6 @@ const returnLocalAndType = (arrayLocation) => {
 };
 
 function getAnimalMap(options) {
-  // seu código aqui
   const location = ['NE', 'NW', 'SE', 'SW'];
   const obj = {};
   if (!options) return returnLocalAndType(location);
@@ -136,7 +126,6 @@ function getAnimalMap(options) {
 }
 
 function getSchedule(dayName) {
-  // seu código aqui
   const { Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday } = hours;
   const obj = {
     Tuesday: `Open from ${Tuesday.open}am until ${Tuesday.close - 12}pm`,
@@ -153,7 +142,6 @@ function getSchedule(dayName) {
 }
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
   const objId = employees.find((value) => (value.id === id));
   const { responsibleFor } = objId;
   const lookingFor = responsibleFor[0];
@@ -165,7 +153,6 @@ function getOldestFromFirstSpecies(id) {
 }
 
 function increasePrices(percentage) {
-  // seu código aqui
   if (!percentage) return 'Nenhum valor passado';
   const percentual = (percentage / 100) + 1;
   prices.Adult = Math.round((prices.Adult * percentual) * 100) / 100;
@@ -188,7 +175,6 @@ const checkSpecies = (responsible) => {
 };
 
 function getEmployeeCoverage(idOrName) {
-  // seu código aqui
   if (!idOrName) {
     const newObj = employees.reduce((acc, curr) => {
       acc[`${curr.firstName} ${curr.lastName}`] = checkSpecies(curr.responsibleFor);
